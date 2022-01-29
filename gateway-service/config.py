a30f028ee47b11eb9a1bab772939fb35d98cda89
impiyush83 <nalawadepiyush@gmail.com>
2022-01-29 07:33:23 -0500

    Gateway service setup

---
+++ gateway-service/config.py
@@ -0,0 +1,11 @@
+URLS = {
+    'production': {
+    },
+    'development': {
+        'USER_SERIVCE_URL': 'http://localhost:5005',
+        'SESSION_SERVICE_URL': 'http://localhost:5004',
+        'WEATHER_DATA_SERVICE_URL': 'http://localhost:5003',
+        'PLOT_SERVICE_URL': 'http://localhost:5002',
+        'AUDIT_SERVICE_URL': 'http://localhost:5001'
+    }
+}
