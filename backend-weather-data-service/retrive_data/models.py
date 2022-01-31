5302cdc5eaa89e1b8f533691d436cf308baa088b
RUSHIKESH PHARATE <35659224+rushikeshpharate@users.noreply.github.com>
2022-01-30 23:32:01 -0500

    Mongo DB created for storing s3 bucket link for given request data

--- backend-weather-data-service/retrive_data/models.py
+++ backend-weather-data-service/retrive_data/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from pymongo import MongoClient
 
-# Create your models here.
+uri = "mongodb://localhost:27017"
+db = MongoClient(uri)['request-service']
+# Add collections in this list
+collections = ['requests']
+
+for collection in collections:
+    if collection in db.list_collection_names():
+        print(collection + ' exists')
+    else:
+        db[collection]
--- backend-weather-data-service/retrive_data/views.py
+++ backend-weather-data-service/retrive_data/views.py
@@ -1,9 +1,16 @@
 from ast import Try
-import re
+import imp
+
+
 # from resource import prlimit
 from turtle import pd
+import uuid
 from django.shortcuts import render
 from django.conf import settings
+
+# for calling plotting microservice
+import requests
+
 
 # For downloading the NEXRAD data
 import nexradaws
@@ -15,42 +22,56 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
+from .models import db
+import uuid
+from datetime import datetime
+
 # Create your views here.
 
 class RetriveData(APIView):
     def get(self,request):
         try:
-            
             date = request.data.get("date")
             time = request.data.get("time")
             station = request.data.get("station")
-           
-            # print(date,time,station)
+            user_id = request.data.get("user_id")
             temp=date.split("-")
             year,month,day=temp[0],temp[1],temp[2]
             temp=time.split(":")
             hh,mm=temp[0],temp[1]
-            
             time_str=str(day)+'/'+str(month)+'/'+str(year)+' '+str(hh)+':'+str(mm)+':'+str(0.0)
             date_format_str = '%d/%m/%Y %H:%M:%S.%f'
-            given_time=datetime.strptime(time_str, date_format_str)
-
-            start_time=given_time-timedelta(minutes=5)
-            end_time=given_time+timedelta(minutes=5)
-
+            given_timestamp=datetime.strptime(time_str, date_format_str)
+            
+            start_time=given_timestamp-timedelta(minutes=5)
+            end_time=given_timestamp+timedelta(minutes=5)
             conn = nexradaws.NexradAwsInterface()
-            templocation = tempfile.mkdtemp()
+            # templocation = tempfile.mkdtemp()
+            try:
+                availscans = conn.get_avail_scans_in_range(start_time,end_time,station)
+                # results = conn.download(availscans[0],templocation)
+                s3_bucket_link='https://s3.amazonaws.com/noaa-nexrad-level2/'
+                collection = db['requests']
+               
+                url=s3_bucket_link+availscans[0].key
+                # print(url)
+                # import pdb
+                # pdb.set_trace()
+                collection.insert_one({
+                    's3_link': url,
+                    'user_id': user_id,
+                    'created_at': datetime.utcnow(),
+                    'updated_at': datetime.utcnow(),
+                    'uuid': str(uuid.uuid4())
+                })
 
-            availscans = conn.get_avail_scans_in_range(start_time,end_time,station)
-           
-            results = conn.download(availscans[0],templocation)
-            print(settings.BASE_DIR)
-            # import pdb
-            # pdb.set_trace()
-
-            return Response({"message":"Success"},status=status.HTTP_200_OK)
-        except:
-            return Response({"message":"Something went wrong"},status=status.HTTP_404_NOT_FOUND)
+                return Response({"message":"Success"},status=status.HTTP_200_OK)
+            except Exception as err:
+                return Response({"message":err},status=status.HTTP_404_NOT_FOUND)
+        except Exception as err:
+            # print("Except block")
+            return Response({"message":err},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         
 
