f55e5eb261357b6077d39e39f50fd169616564bb
BECOB257_Aishwarya Sinhasane <avsinhas@iu.edu>
2022-02-02 17:30:49 -0500

    session service

---
+++ SessionServiceApplication/src/main/java/com/example/SessionService/SessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SessionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceApplication.class, args);
+	}
+
+}
---
+++ SessionServiceApplication/src/test/java/com/example/SessionService/SessionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SessionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
