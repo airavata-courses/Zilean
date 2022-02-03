1a6b0f4dd211830d0fde4b1ef3b83b38c2437d27
impiyush83 <nalawadepiyush@gmail.com>
2022-02-03 09:23:41 -0500

    refactor best practices

--- user-service/app.py
+++ user-service/app.py
@@ -12,17 +12,15 @@ db = MongoClient(uri)['user-service']
 
 @app.route("/v1/user/signup", methods=["POST"])
 def register():
-    import pdb
-    pdb.set_trace()
-    email = request.json["email"]
+    email = request.json.get("email")
     existingUser = db.user.find_one({"email": email})
         
     if existingUser:
         return jsonify(message="User with email {email} Already Exist".format(email=email)), 409
 
-    hash_password = generate_password_hash(request.json["password"])
+    hash_password = generate_password_hash(request.json.get("password"))
     db.user.insert_one({
-        "email": request.json["email"],
+        "email": request.json.get("email"),
         "password": hash_password,
         "uuid": uuid4(),
         "updated_at": moment.utcnow(),
@@ -33,18 +31,20 @@ def register():
 
 @app.route("/v1/user/login", methods=["GET"])
 def login():
-    email = request.json["email"]
-    password = request.json["password"]
+    email = request.json.get("email")
+    password = request.json.get("password")
 
     existingUser = db.user.find_one({"email": email})
 
     if not existingUser:
         return jsonify(message="User does not exist"), 401
 
-    if not check_password_hash(existingUser["password"], password):
+    if not check_password_hash(existingUser.get("password"), password):
         return jsonify(message="Bad password"), 401
 
-    return jsonify(message="User logged in sucessfully"), 200
+    return jsonify({
+        "user_id": existingUser.get("uuid")
+    }), 200
         
 
 if __name__ == '__main__':
