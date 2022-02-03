bf2b2b4bea165c2f85b3e22a12b8a4185d94fd37
impiyush83 <nalawadepiyush@gmail.com>
2022-02-03 08:41:40 -0500

    Fixed signup api

--- user-service/app.py
+++ user-service/app.py
@@ -17,28 +17,28 @@ app = Flask(__name__)
 uri = "mongodb://localhost:27017"
 db = MongoClient(uri)['user-service']
 
-@app.route("/v1/signup", methods=["GET"])
+@app.route("/v1/user/signup", methods=["POST"])
 def register():
-    _email = request.json["email"]
-    dbemail = db.Registration_data.find_one({"email": _email})
+    email = request.json["email"]
+    existingUser = db.users.find_one({"email": email})
         
-    if dbemail:
-        return jsonify(message="User Already Exist"), 409
-    else:
-        hash_password = generate_password_hash(request.json["password"])
-        dc = {"name" : request.json["name"], 
-                           "email":request.json["email"],
-                           "password":hash_password}
-        db.Registration_data.insert_one(dc)
-        return jsonify(message="User registered sucessfully"), 201
+    if existingUser:
+        return jsonify(message="User with email {email} Already Exist".format(email=email)), 409
+
+    hash_password = generate_password_hash(request.json["password"])
+    db.users.insert_one({
+        "email": request.json["email"],
+        "password": hash_password
+    })
+    return jsonify(message="User registered sucessfully"), 201
 
 
-@app.route("/v1/login", methods=["GET"])
+@app.route("/v1/user/login", methods=["GET"])
 def login():
     _email = request.json["email"]
     _password = request.json["password"]
 
-    dbemail = db.Registration_data.find_one({"email": _email})
+    dbemail = db.users.find_one({"email": _email})
 
     if dbemail['email'] == _email:
         if check_password_hash(dbemail["password"], _password):
