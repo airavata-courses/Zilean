681124f75b56f715a1ba0620502f970e95b888e1
impiyush83 <nalawadepiyush@gmail.com>
2022-02-03 09:03:13 -0500

    Integrated Login API with GW service

--- gateway-service/routes/user.py
+++ gateway-service/routes/user.py
@@ -1,5 +1,3 @@
-import imp
-from turtle import pd
 import requests
 from flask import Blueprint, request, jsonify
 import sys
@@ -13,7 +11,7 @@ USER_SERVICE=os.environ.get('USER_SERVICE')
 user_api = Blueprint('user_api', __name__)
 
 
-@user_api.route('/v1/signup', methods=["POST"])
+@user_api.route('/v1/user/signup', methods=["POST"])
 def createUser():
     """
     Used to create an user
@@ -21,15 +19,33 @@ def createUser():
     @return - json of the created user with an http status code
     """
     try:
-        import pdb
-        pdb.set_trace()
         response = requests.post(
-            f'{USER_SERVICE}/v1/signup',
+            f'{USER_SERVICE}/v1/user/signup',
             headers=request.headers,
             data=request.data
         )
         response.raise_for_status()
         return response.json(), response.status_code
+
+    except requests.exceptions.HTTPError as err:
+        return err.response.text, err.response.status_code
+
+
+@user_api.route('/v1/user/login', methods=["GET"])
+def checkUser():
+    """
+    Used to check user exists
+    @params - A GET request sent to the user service to check user exists
+    @return - json of the existing user with an http status code
+    """
+    try:
+        user_service_response = requests.get(
+            f'{USER_SERVICE}/v1/user/login',
+            headers=request.headers,
+            data=request.data
+        )
+        user_service_response.raise_for_status()
+        return user_service_response.json(), user_service_response.status_code
 
     except requests.exceptions.HTTPError as err:
         return err.response.text, err.response.status_code
