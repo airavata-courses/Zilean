5e29b20f397492c7ae8ea80d45000f307016371e
impiyush83 <nalawadepiyush@gmail.com>
2022-02-04 07:04:29 -0500

    Added kafka compatibiltiy

---
+++ data-retrieval-service/retrive_data/kafka_producer.py
@@ -0,0 +1,4 @@
+
+from kafka import KafkaProducer
+
+kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
--- data-retrieval-service/retrive_data/views.py
+++ data-retrieval-service/retrive_data/views.py
@@ -1,12 +1,15 @@
 # from resource import prlimit
+from importlib import import_module
 import uuid
 from django.shortcuts import render
 from django.conf import settings
 
+
 # for calling plotting microservice
 import requests
-
-
+from bson import json_util
+import json
+from .kafka_producer import kafka_producer
 # For downloading the NEXRAD data
 import nexradaws
 from datetime import datetime
@@ -37,17 +40,20 @@ class RetriveData(APIView):
             conn = nexradaws.NexradAwsInterface()
             availscans = conn.get_avail_scans_in_range(start_time,end_time,station)
             url= 'https://s3.amazonaws.com/noaa-nexrad-level2/' + availscans[0].key
-            db['requests'].insert_one({
+            request = db['requests'].insert_one({
                 's3_link': url,
                 'user_id': user_id,
                 'created_at': datetime.utcnow(),
                 'updated_at': datetime.utcnow(),
                 'uuid': str(uuid.uuid4())
             })
+            kafka_producer.send(
+                'plot-queue', 
+                json.dumps({
+                    "request_id": request.uuid,
+                    "s3_link": request.s3_link,
+                }, default=json_util.default
+            ).encode('utf-8'))
             return Response({"message":"Success"},status=status.HTTP_200_OK)
         except Exception as err:
             return Response({"message":err},status=status.HTTP_400_BAD_REQUEST)
-
-        
-
-
