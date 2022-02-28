8563dd7743be053b34040f208ca01a935c848a68
RUSHIKESH PHARATE <35659224+rushikeshpharate@users.noreply.github.com>
2022-02-28 00:18:11 -0500

    Dockerising

--- data-retrieval-service/retrive_data/kafka_producer.py
+++ data-retrieval-service/retrive_data/kafka_producer.py
@@ -1,4 +1,5 @@
+import os
 
 from kafka import KafkaProducer
 
-kafka_producer = KafkaProducer(bootstrap_servers='localhost:29092')
+kafka_producer = KafkaProducer(bootstrap_servers= os.getenv('KAFKA_BROKER_URL') or 'localhost:29092')
