dd43334e79fbba52fd49f4e7ab0d9fba9c8ef5a8
Rushikesh Pharate <35659224+RushikeshPharate@users.noreply.github.com>
2022-03-01 16:13:55 -0500

    / ----> \ for windows setup

--- zilean_windows_setup.sh
+++ zilean_windows_setup.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 git clone -b main-gateway-service https://github.com/airavata-courses/Zilean.git
-move Zilean/gateway-service .
+move Zilean\gateway-service .
 del  Zilean
 
 
 git clone -b main-audit-service https://github.com/airavata-courses/Zilean.git
-move Zilean/audit-service .
+move Zilean\audit-service .
 del  Zilean
 
 
 git clone -b main-user-service https://github.com/airavata-courses/Zilean.git
-move Zilean/user-service .
+move Zilean\user-service .
 del  Zilean
 
 
 git clone -b main-session-service https://github.com/airavata-courses/Zilean.git
-move Zilean/session-service .
+move Zilean\session-service .
 del  Zilean
 
 
 git clone -b main-data-retrieval-service https://github.com/airavata-courses/Zilean.git
-move Zilean/data-retrieval-service .
+move Zilean\data-retrieval-service .
 del  Zilean
 
 
 git clone -b main-plot-service https://github.com/airavata-courses/Zilean.git
-move Zilean/plot-service .
+move Zilean\plot-service .
 del  Zilean
 
 git clone -b main-kafka-audit-queue https://github.com/airavata-courses/Zilean.git
-move Zilean/kafka-audit-queue .
+move Zilean\kafka-audit-queue .
 del  Zilean
 
 git clone -b main-kafka-data-retrieval-queue https://github.com/airavata-courses/Zilean.git
-move Zilean/kafka-data-retrieval-queue .
+move Zilean\kafka-data-retrieval-queue .
 del  Zilean
 
 git clone -b main-kafka-plot-queue https://github.com/airavata-courses/Zilean.git
-move Zilean/kafka-plot-queue .
+move Zilean\kafka-plot-queue .
 del  Zilean
 
 git clone -b main-frontend-service https://github.com/airavata-courses/Zilean.git
-move Zilean/frontend .
+move Zilean\frontend .
 del  Zilean
 
