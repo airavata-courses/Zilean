e08ceee290c7772809652fb8fddceced3c874327
RUSHIKESH PHARATE <35659224+rushikeshpharate@users.noreply.github.com>
2022-03-04 16:52:30 -0500

    Added deploy yaml files

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-kubectl delete deployments --ignore-not-found=true user-container session-container audit-container
-kubectl delete services --ignore-not-found=true user-container session-container audit-container
+# kubectl delete deployments --ignore-not-found=true user-container session-container audit-container postgresql-container
+# kubectl delete services --ignore-not-found=true user-container session-container audit-container postgresql-container
 
 kubectl apply -f deployments/user-container.yaml
 kubectl apply -f deployments/session-container.yaml
 kubectl apply -f deployments/audit-container.yaml
+kubectl apply -f deployments/postgresql-container.yaml
+kubectl apply -f deployments/mongodb-container.yaml
+kubectl apply -f deployments/localstack-container.yaml
+kubectl apply -f deployments/plot-container.yaml
