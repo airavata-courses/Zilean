c2bf98fb5ed8f07a804cc30b47fb57c20a32cb40
impiyush83 <nalawadepiyush@gmail.com>
2022-03-30 01:24:03 -0400

    chore: refactor fetch weather api

--- gateway-service/routes/data_retrieval.py
+++ gateway-service/routes/data_retrieval.py
@@ -25,41 +25,43 @@ def createWeatherData():
     @params - A POST request sent to the data retrieval service to create a new weather retrieval record
     @return - json of the created data retrieval with an http status code
     """
+
+    def nexrad(user_id, request_body):
+        return {
+            "user_id": user_id,
+            "type": request_body.get('type'),
+            "date": request_body.get('date'),
+            "time": request_body.get('time'),
+            "station":request_body.get('station')
+        }
+
+    def merra(user_id, request_body):
+        return {
+            "user_id": user_id,
+            "type": request_body.get('type'),
+            "date": request_body.get('date'),
+        }
+
     try:
         session_service_response = check_user_session(request.headers.get('Access-Token'))
         request_body = json.loads(request.data)
-        type = request_body.get('type')
         user_id = session_service_response.get('user_id')
-        if type == 'nexrad':
+        context_switcher = {
+            'nexrad': nexrad,
+            'merra': merra
+        }
+
+        if request_body.get('type') not in context_switcher.keys():
+            raise Exception('Invalid request type')
         
-            kafka_producer.send('data-retrieval-queue', json.dumps({
-                "user_id": user_id,
-                "type": request_body.get('type'),
-                "date": request_body.get('date'),
-                "time": request_body.get('time'),
-                "station":request_body.get('station')
-            }, default=json_util.default).encode('utf-8'))
-            
-            kafka_producer.send('audit-queue', json.dumps({
+        drq_message ,audit_message =  context_switcher.get(request_body.get('type'))(user_id, request_body), {
                 "user_id": user_id,
                 "response": {},
                 "request": request.data,
                 "service_provider_identifier": 'data-retrieval-service'
-            }, default=json_util.default).encode('utf-8'))   
-        else:
-            kafka_producer.send('data-retrieval-queue', json.dumps({
-                "user_id": user_id,
-                "type": request_body.get('type'),
-                "date": request_body.get('date'),
-            }, default=json_util.default).encode('utf-8'))
-            
-            kafka_producer.send('audit-queue', json.dumps({
-                "user_id": user_id,
-                "response": {},
-                "request": request.data,
-                "service_provider_identifier": 'data-retrieval-service'
-            }, default=json_util.default).encode('utf-8'))   
-        
+        }
+        kafka_producer.send('data-retrieval-queue', json.dumps(drq_message, default=json_util.default).encode('utf-8'))    
+        kafka_producer.send('audit-queue', json.dumps(audit_message, default=json_util.default).encode('utf-8'))   
         return {"message": "You request will be processed in short time"}, 200
     except requests.exceptions.HTTPError as err:
         return err.response.text, err.response.status_code
