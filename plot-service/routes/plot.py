70a30ee7a6d70c46f482457af24e3d87c7d02975
impiyush83 <nalawadepiyush@gmail.com>
2022-03-31 20:36:35 -0400

    Added Merra feature

--- plot-service/routes/plot.py
+++ plot-service/routes/plot.py
@@ -3,26 +3,16 @@ from flask import Blueprint, request
 import boto3
 import botocore
 from botocore.client import Config
-# from sympy import im
 from metpy.io import Level2File
-from utils.plot import plot_data
-from botocore.exceptions import ClientError
+from utils.plot import plot_nexrad_data, plot_merra_data, convert_merra_data
 import os
 from db.models import db
 from datetime import datetime
 
 plot_api = Blueprint('plot_api', __name__)
 
-
-@plot_api.route('/v1/plots', methods=["POST"])
-def create_plot():
-    """
-    Used to create an plot and store its s3 link
-    @params - A POST request sent to the plot service to create a plot of weather data
-    @return - json of the created plot image s3 link
-    """
+def nexrad(request_data):
     try:
-        request_data = request.get_json()
         request_id = request_data.get('request_id')
         user_id = request_data.get('user_id')
         target_link = request_data.get('s3_link')
@@ -60,7 +50,7 @@ def create_plot():
             )
             for obj in s3.Bucket('noaa-nexrad-level2').objects.filter(Prefix=target_link.split('noaa-nexrad-level2/')[1]):
                 f = Level2File(obj.get()['Body'])
-                plot_data(f, request_id)
+                plot_nexrad_data(f, request_id)
                 with open(f'{request_id}.png') as pltfile:
                     if bool(os.environ.get('USE_LOCAL')):
                         endpoint_url = os.getenv("S3_HOST") or 'http://localhost:4566'
@@ -126,6 +116,144 @@ def create_plot():
             "message": str(err)
         }
 
+def merra(request_data):
+    try:
+        request_id = request_data.get('request_id')
+        user_id = request_data.get('user_id')
+        target_link = request_data.get('url')
+        original_request = request_data.get("original_request")
+        plot_link = None
+        year, month, day = original_request['date'].split("-") 
+        FILENAME = year+"-"+month+"-"+day+".nc4"
+        
+
+        existing_plot = db['plots'].find_one({
+            "target_link": target_link,
+            "status": "PROCESSED"
+        })
+        if existing_plot:
+            db['plots'].insert_one({
+            'plot_link': existing_plot['plot_link'],
+            'target_link': existing_plot['target_link'],
+            'user_id': user_id,
+            'created_at': datetime.utcnow(),
+            'updated_at': datetime.utcnow(),
+            'request_id': request_id,
+            'status': existing_plot['status'],
+            'original_request': original_request
+            })
+            return {
+                "message": "Success"
+            }   
+
+        if target_link == 'MERRA-LINK-NOT-FOUND':
+            plot_link = 'MERRA-LINK-NOT-FOUND'
+        else:
+            s3 = boto3.resource(
+                's3',
+                config=Config(
+                    signature_version=botocore.UNSIGNED,
+                    user_agent_extra='Resource'
+                )
+            )
+            result = requests.get(target_link)
+            try:
+                result.raise_for_status()
+                f = open(FILENAME,'wb')
+                f.write(result.content)
+                f.close()
+                print('contents of URL written to '+FILENAME)
+            except:
+                print('requests.get() returned an error code '+str(result.status_code))
+            FILENAME = convert_merra_data(FILENAME)
+            plot_merra_data(FILENAME, request_id)
+            with open(f'{request_id}.png') as pltfile:
+                if bool(os.environ.get('USE_LOCAL')):
+                    endpoint_url = os.getenv("S3_HOST") or 'http://localhost:4566'
+                else:
+                    endpoint_url = os.getenv("S3_HOST")
+                boto3.setup_default_session(aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+                client = boto3.client(
+                    "s3", 
+                    region_name=os.environ.get('AWS_REGION'),
+                    endpoint_url=endpoint_url,
+                )
+                response = client.upload_file(
+                    f'{request_id}.png',
+                    os.getenv('S3_BUCKET'),
+                    f'{request_id}.png',
+                    ExtraArgs={'ACL': 'public-read'}
+                )
+
+                if bool(os.environ.get('USE_LOCAL')):
+                    plot_link = f'http://localhost:4566/plots/{request_id}.png'
+                else:
+                    bucket_location = boto3.client('s3').get_bucket_location(Bucket=os.getenv('S3_BUCKET'))                 
+                    plot_link = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
+                        bucket_location['LocationConstraint'],
+                        os.getenv('S3_BUCKET'),
+                        f'{request_id}.png'
+                    )
+
+        stat =  'UNKNOWN_ERROR'
+        if plot_link == 'MERRA-LINK-NOT-FOUND':
+            stat =  'DATA_RETRIEVAL_FAILURE'
+        elif plot_link == '' or  plot_link == None:
+            stat = 'IMAGE_PARSING_FAILURE'
+        else:
+            stat = 'SUCCESS'
+        db['plots'].insert_one({
+            'plot_link': '' if not plot_link else plot_link,
+            'target_link': target_link,
+            'user_id': user_id,
+            'created_at': datetime.utcnow(),
+            'updated_at': datetime.utcnow(),
+            'request_id': request_id,
+            'status': stat,
+            'original_request': original_request
+        })          
+        return {
+            "message": "Success"
+        }
+    except Exception as err:
+        db['plots'].insert_one({
+            'plot_link': 'NEXRAD-S3-LINK-NOT-FOUND',
+            'user_id': user_id,
+            'target_link': target_link,
+            'created_at': datetime.utcnow(),
+            'updated_at': datetime.utcnow(),
+            'request_id': request_id,
+            'status': 'IMAGE_PARSING_FAILURE',
+            'original_request': original_request
+        })      
+        print(err)
+        return {
+            "message": str(err)
+        }
+
+@plot_api.route('/v1/plots', methods=["POST"])
+def create_plot():
+    """
+    Used to create an plot and store its s3 link
+    @params - A POST request sent to the plot service to create a plot of weather data
+    @return - json of the created plot image s3 link
+    """
+    request_data = request.get_json()
+    type = request_data.get("type")
+    context_switcher = {
+            'nexrad': nexrad,
+            'merra': merra
+        }
+    try:
+        drsmessage = context_switcher.get(type)(request_data)
+        return drsmessage
+    except Exception as err:
+        print(err)
+        return {
+            "message": str(err)
+        }
+    
 
 @plot_api.route('/v1/plots', methods=["GET"])
 def get_plots():
--- plot-service/utils/plot.py
+++ plot-service/utils/plot.py
@@ -1,12 +1,14 @@
-from urllib import request
 import matplotlib
 matplotlib.pyplot.switch_backend('agg')
 import matplotlib.pyplot as plt
 from metpy.plots import add_metpy_logo, add_timestamp
 import numpy as np
+from netCDF4 import Dataset
+import os
+import cartopy.crs as ccrs
 
 
-def plot_data(f, request_id):
+def plot_nexrad_data(f, request_id):
     sweep = 0
     az = np.array([ray[0].az_angle for ray in f.sweeps[sweep]])
     diff = np.diff(az)
@@ -46,3 +48,47 @@ def plot_data(f, request_id):
         add_timestamp(ax, f.dt, y=0.02, high_contrast=True)
     plt.savefig(f'{request_id}.png')
     plt.close()
+
+
+def plot_merra_data(file, request_data):
+    merra_data= Dataset(file, mode='r')
+    request_id = request_data.get('request_id')
+    fig = plt.figure(figsize=(8,4))
+    ax = plt.axes(projection=ccrs.Robinson())
+    ax.set_global()
+    ax.coastlines(resolution="110m",linewidth=1)
+    ax.gridlines(linestyle='--',color='black')
+
+    lons = merra_data.variables['lon'][:]
+    lats = merra_data.variables['lat'][:]
+    T2M = merra_data.variables['TLML'][:,:,:]  #surface specific humidity 
+    T2M = T2M[0,:,:]
+
+    # Set contour levels, then draw the plot and a colorbar
+    contour_levels = np.arange(230,311,5)
+    plt.contourf(lons, lats, T2M, contour_levels, transform=ccrs.PlateCarree(),cmap=plt.cm.jet)
+    plt.title('MERRA-2 Air Temperature at 2m, January 2010', size=14)
+    color_bar = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16, shrink=0.8)
+    color_bar.set_label('K',size=12,rotation=0,labelpad=15)
+    color_bar.ax.tick_params(labelsize=10)
+
+    plt.savefig(f'{request_id}.png')
+
+
+def convert_merra_data(file, request_data):
+
+    hour = int(request_data.get('hour'))
+    merra_data = Dataset(file, mode='r')
+    T2M = merra_data.variables['TLML'][:,:,:]
+    T2M = T2M[hour,:,:]
+
+    filename = file + ".csv"
+
+    data = np.array(T2M)
+
+    np.savetxt(filename, data,delimiter=",")
+    data.close()
+    os.remove(os.getcwd()+"/"+file)
+    print('File Removed')
+
+    return filename
